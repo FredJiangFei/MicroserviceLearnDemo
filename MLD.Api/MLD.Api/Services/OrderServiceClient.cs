f74f9bab4548cb5a88d9ccf4d3c6755ccc68407b
Fei Jiang <jiangf@shinetechchina.com>
2019-09-02 22:40:50 +0800

    MLD Api

--- MLD.Api/MLD.Api/Services/OrderServiceClient.cs
+++ MLD.Api/MLD.Api/Services/OrderServiceClient.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Linq;
+using System.Net.Http;
+using Consul;
 using Grpc.Core;
 
 namespace MLD.Api.Services
@@ -8,7 +12,13 @@ namespace MLD.Api.Services
 
         static OrderServiceClient()
         {
-            var channel = new Channel("127.0.0.1:40001", ChannelCredentials.Insecure);
+            var consulClient = new ConsulClient(c => c.Address = new Uri("http://127.0.0.1:8500"));
+            var services = consulClient.Agent.Services().Result.Response;
+            var orderService = services.FirstOrDefault(x => x.Value.Service == "OrderApi");
+
+            var channel = new Channel(
+                $"{orderService.Value.Address}:{orderService.Value.Port}", 
+                ChannelCredentials.Insecure);
             Client = new OrderService.OrderServiceClient(channel);
         }
 
--- MLD.Api/MLD.Service.Order/OrderRpcConfig.cs
+++ MLD.Api/MLD.Service.Order/OrderRpcConfig.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Consul;
 using Grpc.Core;
 using MLD.Service.Order.Services;
 
@@ -8,18 +9,35 @@ namespace MLD.Service.Order
     {
         public static void Start()
         {
+            var address = "127.0.0.1";
+            var port = 40001;
             var server = new Server
             {
                 Services = { OrderService.BindService(new OrderServiceImpl()) },
-                Ports = { new ServerPort("localhost", 40001, ServerCredentials.Insecure) }
+                Ports = { new ServerPort(address, port, ServerCredentials.Insecure) }
             };
             server.Start();
 
-            Console.WriteLine("grpc ServerListening On Port 40001");
+            ConsulConfig(address, port);
+
+            Console.WriteLine($"grpc ServerListening On Port {port}");
             Console.WriteLine("任意键退出...");
             Console.ReadKey();
 
             server.ShutdownAsync().Wait();
+        }
+
+        private static void ConsulConfig(string address, int port)
+        {
+            var registration = new AgentServiceRegistration()
+            {
+                Address = address,
+                ID = $"OrderApi_{address}:{port}",
+                Name = "OrderApi",
+                Port = port
+            };
+            var consul = new ConsulClient();
+            consul.Agent.ServiceRegister(registration).GetAwaiter().GetResult();
         }
     }
 }
